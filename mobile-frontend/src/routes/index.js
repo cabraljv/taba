7cb3a6e797d5c5c66462d9666bc76c28db5bce2e
Henrique Cotta <sndmnn@outlook.com>
2020-05-02 19:20:20 -0300

    Implemented application routes

---
+++ mobile-frontend/src/routes/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import UserProfile from '../pages/UserProfile';
+import EstablishmentProfile from '../pages/EstablishmentProfile';
+
+import Cart from '../pages/Cart';
+import Orders from '../pages/Orders';
+
+import Dashboard from '../pages/Dashboard';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#dedede' },
+    }}>
+    <Auth.Screen name="UserProfile" component={UserProfile} />
+    <Auth.Screen name="EstablishmentProfile" component={EstablishmentProfile} />
+
+    <Auth.Screen name="Cart" component={Cart} />
+    <Auth.Screen name="Orders" component={Orders} />
+
+    <Auth.Screen name="SignIn" component={SignIn} />
+    <Auth.Screen name="SignUp" component={SignUp} />
+
+    <Auth.Screen name="Dashboard" component={Dashboard} />
+  </Auth.Navigator>
+);
+
+export default AuthRoutes;
